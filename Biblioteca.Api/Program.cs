dc881d32f3ae57bc8b5eef3ee886c138f0505d53
João Vianney <joaoplay318@gmail.com>
2026-08-20 20:07:21 -0300

    feat: projeto de api na solucao, com referencia ao dominio e endpoint padrao subindo

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -1,6 +1,10 @@
+using Biblioteca.Dominio;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+ItemAcervo Livro = new Livro("Titulo","Autor");
 
 app.Run();
